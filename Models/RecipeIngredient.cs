a056230913bd2941f42f5891f50ea3912a54085b
eChadwick <en.chadwick@gmail.com>
2020-09-26 13:32:23 -0500

    add recipe and recipeIngredient models

---
+++ Models/RecipeIngredient.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeBox2.Models
+{
+    public class RecipeIngredient
+    {
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+        public string Measurement { get; set; }
+    }
+}
